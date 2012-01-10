690d0b2e83cd999b717aa991235328a512969536
peter <spikerbond@gmail.com>
2012-01-10 11:25:42 -0600

    test 3

--- Term 2 project/src/Main.java
+++ Term 2 project/src/Main.java
@@ -1,33 +1,13 @@
 
-import java.awt.BufferCapabilities;
-import java.awt.BufferCapabilities.FlipContents;
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
-import java.awt.Image;
-import java.awt.ImageCapabilities;
 import java.awt.RenderingHints;
-import java.awt.RenderingHints.Key;
-import java.awt.geom.AffineTransform;
 import java.awt.image.*;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Random;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.Timer;
-import javax.swing.JButton;
-
-import com.sun.xml.internal.stream.util.BufferAllocator;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
